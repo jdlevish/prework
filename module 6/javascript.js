b1d01dd84e431cf27ec2ea5476dda41054e9592f
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-04-21 16:14:19 -0700

    added function buttons

--- module 6/javascript.js
+++ module 6/javascript.js
@@ -1,12 +1,18 @@
 document.getElementById("button1").addEventListener("click",function(){
     document.getElementById("box").style.height = "500px";
-})
+});
 document.getElementById("button2").addEventListener("click", function(){
     document.getElementById("box").style.backgroundColor = "blue";
-})
+});
 document.getElementById("button3").addEventListener("click", function(){
     document.getElementById("box").style.opacity = ".2";
-})
+});
 document.getElementById("button4").addEventListener("click", function(){
     document.getElementById("box").style = "height:150px; width:150px; background-color:orange; margin:25px" ;
-})
+});
+document.getElementById("button5").addEventListener("click", function(){
+    document.getElementById("box").style.boxShadow = "10px 10px gray"  ;
+});
+document.getElementById("button6").addEventListener("click", function(){
+    document.getElementById("box").style = "height:600px; width:600px; background-color:#fac97a; margin:25px"  ;
+});
